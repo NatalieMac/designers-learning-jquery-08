d2676199434897b563c26879594c82516ba30c91
Natalie MacLees <natalie@nataliemac.com>
2014-09-30 15:42:05 -0700

    Initial Commit

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+// $('.color5').parent('div').addClass('nocolor');
+// $('.color5').parents('div').addClass('nocolor');
+
+// $('.color3').find('div').addClass('nocolor');
+// $('.color3').children('div').addClass('nocolor');
+// $('.color3').find('div div').addClass('nocolor');
+
+$('main').find('p').addClass('special');
